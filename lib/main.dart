d67e6aedf835f9426373c917dff28cad1f9cf17c
Fahad Karim Khan <fkk420302@gmail.com>
2023-10-21 15:51:37 +0500

    Routes Basics Implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
-import 'homePage.dart';
+import 'pages/login_page.dart';
+import 'pages/homePage.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    String userName = "Fahad"; // Replace "Fahad" with the user's name
     return MaterialApp(
-      home: HomePage(userName: userName),
+      // home: const HomePage(),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        '/': (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+      },
     );
   }
 }
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  final String userName;
-
-  const HomePage({super.key, required this.userName});
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome, $userName!'),
+        title: Text('Woman Safety'),
       ),
       body: Center(
         child: Container(
-          child: Text("Hello, $userName!"),
+          child: Text("Hello, Fahad!"),
         ),
       ),
       drawer: const Drawer()
